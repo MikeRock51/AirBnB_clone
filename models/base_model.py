08858bbfbcf3020c77f8e529e152791f5b5f0bd1
MikeRock51 <mikerockmusic51@gmail.com>
2023-02-07 15:25:41 +0100

    Add BaseModel.__str__() method

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,20 @@
 """This module contains the base class for our project"""
 
 import uuid
+from datetime import datetime
 
-class Base:
-    id = str(uuid.uuid4())
+class BaseModel:
+    """Base class for our project"""
+    
+    def __init__(self, *args, **kwargs):
+        """Base object constructor"""
+
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+    
+    def __str__(self):
+        """Overrides the default __str__ method"""
+        return ("[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__))
+
     
