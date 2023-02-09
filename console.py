a1e774c6adb2e09b112a7e4351a9e05596eaa919
MikeRock51 <mikerockmusic51@gmail.com>
2023-02-09 13:49:50 +0100

    Implement the show command

--- console.py
+++ console.py
@@ -22,6 +22,22 @@ class HBNBCommand(cmd.Cmd):
             instance.save()
             print(instance.id)
 
+    def do_show(self, line):
+        """Prints the string representation of an
+        instance based on the class name and id"""
+
+        line = line.split()
+        if len(line) < 1:
+            print("** class name missing **")
+        elif globals().get(line[0]) is None:
+                print("** class doesn't exist **")
+        elif len(line) < 2:
+                print("** instance id missing **")
+        elif not f"{line[0]}.{line[1]}" in storage.all():
+            print("** no instance found **")
+        else:
+            print(storage.all()[f"{line[0]}.{line[1]}"])
+
     def do_EOF(self, line):
         """Quits the command interpreter when it receives EOF signal"""
         print()
