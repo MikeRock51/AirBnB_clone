7af1d0f6f93b3143d422a8dd0265e975cca4fd44
MikeRock51 <mikerockmusic51@gmail.com>
2023-02-09 11:00:14 +0100

    Quick update: Stepping away for a bit

--- console.py
+++ console.py
@@ -2,11 +2,16 @@
 """Entry point of HBNB command interpreter"""
 
 import cmd
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     """Command interpreter class"""
 
     prompt = '(hbnb) '
+
+    def do_create(self, cls):
+        """Creates a new instance of BaseModel"""
+        pass
 
     def do_EOF(self, line):
         """Quits the command interpreter when it receives EOF signal"""
