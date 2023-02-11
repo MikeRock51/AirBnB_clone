b6b76ec6596366c08211bf424cd1b7c2bbf349fc
MikeRock51 <mikerockmusic51@gmail.com>
2023-02-11 13:21:07 +0100

    File_storage  pep8 formatting

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,6 +6,7 @@ and deserializes JSON file to instances
 import json
 import os
 
+
 class FileStorage:
     """Stores and retrieves data"""
     __file_path = "file.json"
@@ -17,13 +18,15 @@ class FileStorage:
 
     def new(self, obj):
         """Sets obj with the key <obj class name>.id in __objects"""
-        FileStorage.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
+        FileStorage.__objects["{}.{}".format(
+            obj.__class__.__name__, obj.id)] = obj
 
     def save(self):
         """Serializes __objects to the JSON file (path: __file_path)"""
 
         with open(FileStorage.__file_path, "w") as f:
-            objects = {key: value.to_dict() for key, value in FileStorage.__objects.items()}
+            objects = {key: value.to_dict()
+                       for key, value in FileStorage.__objects.items()}
             json.dump(objects, f)
 
     def class_list(self):
