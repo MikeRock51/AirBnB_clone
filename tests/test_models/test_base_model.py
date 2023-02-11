765c811b8834e4af81571194655e2823cedbadb8
MikeRock51 <mikerockmusic51@gmail.com>
2023-02-11 12:51:17 +0100

    test_base check

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,3 +1,41 @@
-import unittest
-"""Test module for the base model"""
+#!/usr/bin/python3
+"""Unittest module for the BaseModel Class."""
 
+from models import storage
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+from datetime import datetime
+import json
+import os
+import re
+import time
+import unittest
+import uuid
+
+
+class TestBaseModel(unittest.TestCase):
+
+    """Test Cases for the BaseModel class."""
+
+    def setUp(self):
+        """Sets up test methods."""
+        pass
+
+    def tearDown(self):
+        """Tears down test methods."""
+        self.resetStorage()
+        pass
+
+    def resetStorage(self):
+        """Resets FileStorage data."""
+        FileStorage._FileStorage__objects = {}
+        if os.path.isfile(FileStorage._FileStorage__file_path):
+            os.remove(FileStorage._FileStorage__file_path)
+
+    def test_3_instantiation(self):
+        """Tests instantiation of BaseModel class."""
+
+        b = BaseModel()
+        self.assertEqual(str(type(b)), "<class 'models.base_model.BaseModel'>")
+        self.assertIsInstance(b, BaseModel)
+        self.assertTrue(issubclass(type(b), BaseModel))
